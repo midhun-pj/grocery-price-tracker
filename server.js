89e0adb6a2c866226185c84f6fbcd0654a9e810c
midhun-pj <midhunjayan.official@gmail.com>
2025-08-22 10:59:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+// config
 import { PORT } from './config.js';
+// routes
 import authRoutes from './routes/authRoutes.js';
 import superMarketRoutes from './routes/superMarketRoutes.js';
 import groceryRoutes from './routes/groceryRoutes.js';
@@ -8,6 +11,13 @@ import groceryRoutes from './routes/groceryRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(cookieParser({
+  secure: process.env.NODE_ENV === 'production', // HTTPS only in production
+  sameSite: 'strict',
+  httpOnly: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/supermarkets', superMarketRoutes);
