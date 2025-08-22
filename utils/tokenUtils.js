483551c58ee6c360b922a3b6299dc00404d8d6ac
midhun-pj <midhunjayan.official@gmail.com>
2025-08-22 11:00:29 +0200

    Create tokenUtils.js

---
+++ utils/tokenUtils.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign(
+    { userId },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN }
+  );
+
+  const refreshToken = jwt.sign(
+    { userId },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN }
+  );
+
+  return { accessToken, refreshToken };
+};
+
+const verifyAccessToken = (token) => {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
+
+module.exports = {
+  generateTokens,
+  verifyAccessToken,
+  verifyRefreshToken
+};
